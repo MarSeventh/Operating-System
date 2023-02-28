9f8c0ec6dd18bfaba25531bd743e0c207f1153f9
黄一轩 <21373347@buaa.edu.cn>
2023-02-28 10:29:32 +0800

    Third

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-/*	int n;
+	int n;
 	scanf("%d", &n);
         char str[10] = {};
 	int i=0;
@@ -16,6 +16,6 @@ int main() {
 		printf("Y\n");
 	} else {
 		printf("N\n");
-	}*/
+	}
 	return 0;
 }
