c4c1ed3de5f27aed1e831affe46d2f0eb30d743c
黄一轩 <21373347@buaa.edu.cn>
2023-03-06 20:21:16 +0800

    Init

--- analyze.sh
+++ analyze.sh
@@ -2,20 +2,20 @@
 
 if [ $# -eq 1 ];
 then
-    # Your code here. (1/4)
+    grep -E "(WARN)|(ERROR)" $1 > bug.txt
 
 else
     case $2 in
     "--latest")
-        # Your code here. (2/4)
+        tail -n 5 $1 
 
     ;;
     "--find")
-        # Your code here. (3/4)
+        grep $3 $1 > $3.txt
 
     ;;
     "--diff")
-        # Your code here. (4/4)
+        diff -sq $1 $3
 
     ;;
     esac
