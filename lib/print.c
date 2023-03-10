eb130a77620628e77c077a96fa3ca5ed6bbd1226
黄一轩 <21373347@buaa.edu.cn>
2023-03-10 21:37:10 +0800

    Second

--- lib/print.c
+++ lib/print.c
@@ -48,6 +48,7 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 		}
 		/* get width */
 		/* Exercise 1.4: Your code here. (6/8) */
+		width = 0;
                 while (*fmt >= '0' && *fmt <= '9'){
 			width = width*10 + *fmt - '0';
 			fmt++;
