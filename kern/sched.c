d20b0a09e4b142e7a1dfd0ed229583a165cced34
黄一轩 <21373347@buaa.edu.cn>
2023-04-17 19:31:21 +0800

    lab3-exam-first

--- kern/sched.c
+++ kern/sched.c
@@ -17,7 +17,8 @@
 void schedule(int yield) {
 	static int count = 0; // remaining time slices of current env
 	struct Env *e = curenv;
-	/* We always decrease the 'count' by 1.
+        static int user_time[5]; // 创建一个用户累计运行时间片数数组
+        /* We always decrease the 'count' by 1.
 	 *
 	 * If 'yield' is set, or 'count' has been decreased to 0, or 'e' (previous 'curenv') is
 	 * 'NULL', or 'e' is not runnable, then we pick up a new env from 'env_sched_list' (list of
@@ -34,15 +35,34 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
+	int user_able[5] = {};
+        struct Env *var;
+	TAILQ_FOREACH(var, &env_sched_list, env_sched_link){
+		if(user_able[var->env_user] == 0){
+			user_able[var->env_user] = 1;
+		}
+	}
         if(yield || count == 0 || e == NULL || e->env_status != ENV_RUNNABLE){
 		if(e!=NULL && e->env_status == ENV_RUNNABLE){
 			TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
 			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+			user_time[e->env_user] += e->env_pri;
 		}
 		if(TAILQ_EMPTY(&env_sched_list)){
 			panic("schedule: no runnable envs\n");
 		}
-		struct Env* new_e = TAILQ_FIRST(&env_sched_list);
+		int next_user = 0;
+		int mintime = 10000;
+		for(int i=0;i<5;i++){
+			if(user_able[i] == 1 && user_time[i] < mintime){
+				mintime = user_time[i];
+				next_user = i;
+			}
+		}
+		struct Env* new_e;
+		TAILQ_FOREACH(new_e, &env_sched_list, env_sched_link){
+			if(new_e->env_user == next_user) break;
+		}
 		count = new_e->env_pri;
 		count--;
 		env_run(new_e);
