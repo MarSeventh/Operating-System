12b57a3e5b7e66b4c0566b6c19a1207bee00d20b
黄一轩 <21373347@buaa.edu.cn>
2023-06-15 16:44:24 +0800

    change_read

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -406,8 +406,9 @@ int sys_ipc_try_send(u_int envid, u_int value, u_int srcva, u_int perm) {
 // XXX: kernel does busy waiting here, blocking all envs
 int sys_cgetc(void) {
 	int ch;
-	while ((ch = scancharc()) == 0) {
-	}
+//	while ((ch = scancharc()) == 0) {
+//	}
+        ch = scancharc();
 	return ch;
 }
 
